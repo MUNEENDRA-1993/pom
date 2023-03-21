9c2487adb5f3df60eaac635e364e7dbed8f9e5f0
muneendraM <muneendra0318@gmail.com>
2023-03-21 17:18:46 +0300

    added some new methods

--- src/main/java/Com/Testing1/Abc.java
+++ src/main/java/Com/Testing1/Abc.java
@@ -4,7 +4,18 @@ public class Abc {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("this is my abc class");
+		
+		compare("Testing","Test");
 
+	}
+	
+	public static void compare(String a,String b) {
+		if(a.equalsIgnoreCase(b)) {
+			System.out.println("both are equal");
+		}else {
+			System.out.println("both are not equal");
+		}
 	}
 
 }
--- src/test/java/Com/Testing1/Base.java
+++ src/test/java/Com/Testing1/Base.java
@@ -5,7 +5,15 @@ public class Base {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		 System.out.println("this is my base class");
+		 
+		 int x=add(25,35);
+		 System.out.println(x);
 
 	}
+	
+	   public static int add(int a, int b) {
+		    return a+b;
+		   
+	   }
 
 }
